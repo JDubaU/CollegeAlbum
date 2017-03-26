3cc40ce5b54b7f2f09d8b297e791b3ac059205ae
Freaking Udemy App 2 <you@yourdomain.com>
2017-03-25 20:02:50 -0500

    Version 2.0

--- CollegeAlbum/HomeViewController.swift
+++ CollegeAlbum/HomeViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
+    @IBOutlet weak var addPhoto: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +22,10 @@ class HomeViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+        
     }
     
 
