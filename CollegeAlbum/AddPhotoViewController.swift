a547ee24270041d3d665a4c4afdba574848c6aa5
Freaking Udemy App 2 <you@yourdomain.com>
2017-04-05 13:16:24 -0500

    Added Delete message and fixed popping navigation controllers

--- CollegeAlbum/AddPhotoViewController.swift
+++ CollegeAlbum/AddPhotoViewController.swift
@@ -8,14 +8,129 @@
 
 import UIKit
 
-class AddPhotoViewController: UIViewController {
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var photoButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var collegeImageView: UIImageView!
+    @IBOutlet weak var addButton: UIButton! //Also Update button
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var subtextTextField: UITextField!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    
+    var imagePicker = UIImagePickerController()
+    var memory : Memory? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        deleteButton.isHidden = true
+        
+        if memory != nil {
+            print("We have liftoff")
+            deleteButton.isHidden = false
+            collegeImageView.image = UIImage(data: memory?.image as! Data)
+            dateTextField.text = memory!.date
+            subtextTextField.text = memory!.subtext
+            
+            addButton.setTitle("Update", for: .normal)
+        } else {
+            
+        }
+        
     }
+    
+    @IBAction func photoButton(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        collegeImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraButton(_ sender: Any) {
+        /*
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+        */
+    }
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        
+        if memory != nil {
+            memory!.date = dateTextField.text
+            memory!.subtext = subtextTextField.text
+            memory!.image = UIImagePNGRepresentation(collegeImageView.image!) as NSData?
+        } else {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let memory = Memory(context: context)
+            
+            memory.date = dateTextField.text
+            memory.subtext = subtextTextField.text
+            memory.image = UIImagePNGRepresentation(collegeImageView.image!) as NSData?
+        }
+        
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+    }
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Delete Message", message: "Are you sure you want to delete this photo?", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: {
+            (alert:UIAlertAction!) in
+            //****//
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(self.memory!)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            self.navigationController!.popViewController(animated: true)
 
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    
+    }
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- CollegeAlbum/HomeViewController.swift
+++ CollegeAlbum/HomeViewController.swift
@@ -8,15 +8,55 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     @IBOutlet weak var addPhoto: UIBarButtonItem!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var memories : [Memory] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            memories = try context.fetch(Memory.fetchRequest())
+            tableView.reloadData()
+        } catch {
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let memory = memories[indexPath.row]
+        cell.textLabel?.text = memory.date
+        cell.imageView?.image = UIImage(data: memory.image as! Data)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let memory = memories[indexPath.row]
+        performSegue(withIdentifier: "memorySegue", sender: memory)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! AddPhotoViewController
+        nextVC.memory = sender as? Memory
     }
 
     override func didReceiveMemoryWarning() {
